279e061086a10e7c13e958f4f31492512491d8df
VictorNalu <victorchinalurumogu@gmail.com>
2024-05-07 08:32:51 +0100

    puppet

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,9 @@
-# This Puppet manifest creates a file in /tmp named "school"
-
+# This puppet manifest creates a file names/tmp/school
 file { '/tmp/school':
-  ensure  => file,
-  mode    => '0744',
-  owner   => 'www-data',
-  group   => 'www-data',
-  content => 'I love Puppet',
+  ensure         => file,
+  mode           => '0744',
+  owner          => 'www-data',
+  group          => 'www-data',
+  content        => 'I love Puppet\n',
+  ensure_newline => true,
 }
-
