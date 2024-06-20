0fc44294e59bf4251dd8fe09f957c6dbbe76a2c7
VictorNalu <victorchinalurumogu@gmail.com>
2024-06-20 21:14:57 +0100

    debugging

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,3 +1,5 @@
+# solves the bad phpp extensions to php in the WordPress file wp-settings.php.
+
 exec { 'fix-wordpress':
   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
   path    => '/usr/local/bin/:/bin/'
